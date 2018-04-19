cfe0939b14ab6f2c7bd59bc6c40fee987e922a42
mkarara91 <mkarara.91@gmail.com>
2018-04-20 00:28:33 +0200

    added game service

--- tictactoe/src/main/java/service/GameServiceImpl.java
+++ tictactoe/src/main/java/service/GameServiceImpl.java
@@ -2,12 +2,33 @@ package service;
 
 import java.util.List;
 
+import model.Board;
 import model.Game;
 import model.Player;
 
 public class GameServiceImpl implements GameService{
 
 	public Game setupGame(List<Player> players, int boardSize) {
+		Board board = new Board(boardSize);
+		return new Game(players, board);
+	}
+
+	public Player checkForWinner(Game game) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Player checkRowsForWinner(Board board) {
+		
+		return null;
+	}
+
+	public Player checkColumnsForWinner(Board board) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Player checkDiagonalForWinner(Board board) {
 		// TODO Auto-generated method stub
 		return null;
 	}
